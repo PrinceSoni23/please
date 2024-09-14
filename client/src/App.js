aa4c27c743b49173912b6b08c7a37c738f0a2527
Prince <princespno21332@gmail.com>
2024-09-14 21:59:51 +0530

    Initial commit - added React

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import ItineraryPage from './ItineraryPage';
+
+function App() {
+  return (
+    <div className="App">
+      <ItineraryPage />
+    </div>
+  );
+}
+
+export default App;
